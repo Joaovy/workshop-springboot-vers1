39a3717643ad8970f52469b8ae2b4fd38e27b194
João Antonio <joao.visio.ae@gmail.com>
2025-04-09 15:13:23 -0300

    User insert

--- src/main/java/com/projectWebService/webService/resource/UserResource.java
+++ src/main/java/com/projectWebService/webService/resource/UserResource.java
@@ -5,11 +5,10 @@ import com.projectWebService.webService.services.UserServices;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,12 +29,21 @@ public class UserResource {
     public void init() {
         System.out.println("UserResource carregado com sucesso!");
     }
-
+    // Recuperando recurso do banco de dados
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = services.findById(id);
 
         return  ResponseEntity.ok().body(obj);
+    }
+
+    //Inserindo um novo recurso
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = services.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 
--- src/main/java/com/projectWebService/webService/services/UserServices.java
+++ src/main/java/com/projectWebService/webService/services/UserServices.java
@@ -27,5 +27,9 @@ public class UserServices {
 
     }
 
+    public User insert(User obj){
+        return repository.save((obj));
+    }
+
 
 }
